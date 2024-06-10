ce424d4633292e636f868c19b2712cc37a5a45e5
Muhammad Arsalan <ak0256344@gmail.com>
2024-06-10 14:28:27 +0500

    updated complete application

--- android/app/src/debug/java/com/happy_engagement/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/happy_engagement/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.happy_engagement_kiran;
+package com.happy_engagement;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
--- android/app/src/main/java/com/happy_engagement/MainActivity.java
+++ android/app/src/main/java/com/happy_engagement/MainActivity.java
@@ -1,4 +1,4 @@
-package com.happy_engagement_kiran;
+package com.happy_engagement;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Happy_Engagement_Kiran";
+    return "Happy_Engagement";
   }
 
   /**
